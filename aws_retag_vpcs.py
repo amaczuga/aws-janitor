ed2f6e6d9de552bd9915a228b95d12bc60b4039c
Andrzej Maczuga <andrzej.maczuga@intel.com>
2016-11-30 08:48:13 -0500

    Add retagging EC2 volumes

--- aws_retag_vpcs.py
+++ aws_retag_vpcs.py
@@ -28,12 +28,19 @@ def main():
         aws = boto3.session.Session(region_name=aws_region)
         for vpc in aws.resource('ec2').vpcs.all():
             instance_count = sum(1 for e in vpc.instances.all())
+            vpc_items = []
             if valid_tags(vpc.tags, intel_tags) and instance_count > 0:
-                vpc_desc = "%s::%s::%d " % (aws_region, vpc.vpc_id, instance_count)
-                print vpc_desc + "instances tagged."
+#                vpc_desc = "%s::%s::%d " % (aws_region, vpc.vpc_id, instance_count)
+#                print vpc_desc + "instances tagged."
                 selected_tags = [tag for tag in vpc.tags if tag["Key"] in intel_tags.keys()]
                 vpc_inst = [inst.id for inst in vpc.instances.all()]
-                aws.client('ec2').create_tags(Resources=vpc_inst, Tags=selected_tags)
+                vol_fltr = [{'Name': 'attachment.instance-id', 'Values': vpc_inst}]
+                vpc_vols = [vol.id for vol in aws.resource('ec2').volumes.filter(Filters=vol_fltr)]
+                vpc_items.extend(vpc_inst)
+                vpc_items.extend(vpc_vols)
+                aws.client('ec2').create_tags(Resources=vpc_items, Tags=selected_tags)
+                vpc_desc = "%s::%s %d/%d" % (aws_region, vpc.vpc_id, len(vpc_inst), len(vpc_vols))
+                print vpc_desc + " inst/vols tagged."
 
 main()
 
